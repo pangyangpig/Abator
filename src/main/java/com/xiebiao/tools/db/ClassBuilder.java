f91be661716fc409a3f021fccd77a9807a6b956a
xiaog <joyrap@gmail.com>
2011-08-24 17:59:48 +0800

    Deploy 0.0.1-SNAPSHOT.

---
+++ src/main/java/com/xiebiao/tools/db/ClassBuilder.java
@@ -0,0 +1,36 @@
+package com.xiebiao.tools.db;
+
+public class ClassBuilder {
+    protected void buildPackage() {
+
+    }
+
+    protected void buildImport() {
+
+    }
+
+    protected void buildClassName() {
+
+    }
+
+    protected void buildField() {
+
+    }
+
+    protected void buildSetterGetter() {
+
+    }
+
+    protected void buildClassEnd() {
+
+    }
+
+    public final void build() {
+	buildPackage();
+	buildImport();
+	buildClassName();
+	buildField();
+	buildSetterGetter();
+	buildClassEnd();
+    }
+}
---
+++ src/main/java/com/xiebiao/tools/db/FileUtils.java
@@ -0,0 +1,23 @@
+package com.xiebiao.tools.db;
+
+import java.io.File;
+
+public final class FileUtils {
+    public static void deleteDirectory(String filepath) {
+	File f = new File(filepath);
+	if (f.exists() && f.isDirectory()) {
+	    if (f.listFiles().length == 0) {
+		f.delete();
+	    } else {
+		File delFile[] = f.listFiles();
+		int i = f.listFiles().length;
+		for (int j = 0; j < i; j++) {
+		    if (delFile[j].isDirectory()) {
+			deleteDirectory(delFile[j].getAbsolutePath());
+		    }
+		    delFile[j].delete();
+		}
+	    }
+	}
+    }
+}
