d413e458f21ddf8b9626fc03355502ba419e4887
xiaog <joyrap@gmail.com>
2012-04-18 00:07:59 +0800

    可以配置生成的Domain后缀名

--- src/main/java/com/xiebiao/tools/db/Config.java
+++ src/main/java/com/xiebiao/tools/db/Config.java
@@ -16,173 +16,179 @@ import java.util.Properties;
 import java.util.Set;
 
 public class Config {
-    public static final String INFORMATION_SCHEMA = "information_schema";
-    private Properties properties;
-    private static final String CONFIG_FILE = "config.properties";
-    private String _package;
-    private String _extends;
+	public static final String INFORMATION_SCHEMA = "information_schema";
+	private Properties properties;
+	private static final String CONFIG_FILE = "config.properties";
+	private String _package;
+	private String _extends;
+	private String _domainSuffix = "";
 
-    public Config() {
-	if (System.getProperty("db.host") == null
-		|| System.getProperty("db.port") == null
-		|| System.getProperty("db.name") == null
-		|| System.getProperty("db.user") == null
-		|| System.getProperty("db.password") == null) {
-	    properties = new Properties();
-	    try {
-		properties.load(new FileInputStream(
-			new File(System.getProperty("user.dir")
-				+ File.separator + CONFIG_FILE)));
-		this._package = properties.getProperty("package");
-		this._extends = properties.getProperty("extends");
-	    } catch (FileNotFoundException e) {
-		e.printStackTrace();
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
-	} else {
-	    String host = System.getProperty("db.host");
-	    String port = System.getProperty("db.port");
-	    String name = System.getProperty("db.name");
-	    String user = System.getProperty("db.user");
-	    String password = System.getProperty("db.password");
-	    properties = new Properties();
-	    properties.put("db.host", host);
-	    properties.put("db.port", port);
-	    properties.put("db.name", name);
-	    properties.put("db.user", user);
-	    properties.put("db.password", password);
-	    properties.put("package", "");
-	}
-	try {
-	    Class.forName("com.mysql.jdbc.Driver");
-	} catch (ClassNotFoundException e) {
-	    e.printStackTrace();
-	}
-    }
-
-    public Config(String configFile) {
-	properties = new Properties();
-	try {
-	    properties.load(new FileInputStream(new File(configFile)));
-	    this._package = properties.getProperty("package");
-	    this._extends = properties.getProperty("extends");
-	} catch (FileNotFoundException e) {
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-    }
-
-    private Connection getConnection() {
-	try {
-	    String jdbcUrl = "jdbc:mysql://"
-		    + properties.getProperty("db.host") + ":"
-		    + properties.getProperty("db.port") + "/"
-		    + INFORMATION_SCHEMA + "?user="
-		    + properties.getProperty("db.user") + "&password="
-		    + properties.getProperty("db.password");
-	    Connection connection = DriverManager.getConnection(jdbcUrl);
-	    if (connection == null) {
-		System.out.println("Can't get connection");
-	    }
-	    return connection;
-	} catch (SQLException e) {
-	    e.printStackTrace();
-	    System.out.println(e);
-	}
-	return null;
-    }
-
-    private void close(Connection connection) {
-	try {
-	    connection.close();
-	} catch (SQLException e) {
-	    e.printStackTrace();
-	}
-    }
-
-    public String getPackage() {
-	return this._package;
-    }
-
-    public String getExtends() {
-	return this._extends;
-    }
-
-    public void setPackage(String _package) {
-	this._package = _package;
-    }
-
-    public void setExtends(String _extends) {
-	this._extends = _extends;
-    }
-
-    public List<Table> getTables() {
-	Connection connection = this.getConnection();
-	String sql = "select * from TABLES where TABLE_SCHEMA=?";
-	List<Table> tables = null;
-	try {
-	    PreparedStatement preparedStatement = connection
-		    .prepareStatement(sql);
-	    preparedStatement.setString(1,
-		    this.properties.getProperty("db.name"));
-	    ResultSet rs = preparedStatement.executeQuery();
-	    tables = new ArrayList<Table>();
-	    while (rs.next()) {
-		Table table = new Table();
-		table.setName(rs.getString("TABLE_NAME").trim());
-		table.setComment(rs.getString("TABLE_COMMENT").trim());
-		table.setColumns(getColumns(table));
-		tables.add(table);
-	    }
-	    close(connection);
-	} catch (SQLException e) {
-	    e.printStackTrace();
-	}
-	return tables;
-    }
-
-    public Set<Column> getColumns(Table table) {
-	Connection connection = this.getConnection();
-	String sql = "select * from COLUMNS where TABLE_SCHEMA=? and TABLE_NAME=?";
-	Set<Column> columns = null;
-	try {
-	    PreparedStatement preparedStatement = connection
-		    .prepareStatement(sql);
-	    preparedStatement.setString(1,
-		    this.properties.getProperty("db.name"));
-	    preparedStatement.setString(2, table.getName());
-	    ResultSet rs = preparedStatement.executeQuery();
-	    columns = new HashSet<Column>();
-	    boolean hasPriKey = false;
-	    while (rs.next()) {
-		Column column = new Column();
-		column.setName(rs.getString("COLUMN_NAME").trim());
-		column.setDataType(rs.getString("DATA_TYPE"));
-		if (table.getName().equals("digu_single_kitchen_video")) {
-		    System.out.println(table.getName() + "  "
-			    + column.getName() + "  " + column.getDataType());
+	public Config() {
+		if (System.getProperty("db.host") == null
+				|| System.getProperty("db.port") == null
+				|| System.getProperty("db.name") == null
+				|| System.getProperty("db.user") == null
+				|| System.getProperty("db.password") == null) {
+			properties = new Properties();
+			try {
+				properties.load(new FileInputStream(
+						new File(System.getProperty("user.dir")
+								+ File.separator + CONFIG_FILE)));
+				this._package = properties.getProperty("package");
+				this._extends = properties.getProperty("extends");
+				this._domainSuffix = properties.getProperty("domain.suffix");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			String host = System.getProperty("db.host");
+			String port = System.getProperty("db.port");
+			String name = System.getProperty("db.name");
+			String user = System.getProperty("db.user");
+			String password = System.getProperty("db.password");
+			properties = new Properties();
+			properties.put("db.host", host);
+			properties.put("db.port", port);
+			properties.put("db.name", name);
+			properties.put("db.user", user);
+			properties.put("db.password", password);
+			properties.put("package", "");
 		}
-		if (rs.getString("COLUMN_KEY").equalsIgnoreCase("PRI")) {
-		    column.setPrimaryKey(true);
-		    hasPriKey = true;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
-		column.setComment(rs.getString("COLUMN_COMMENT").trim());
-		columns.add(column);
-	    }
-	    if (!hasPriKey) {
-		System.out.println("WARN: table(" + table.getName()
-			+ ") must have PRIMARY KEY. ");
-	    }
-	    close(connection);
-	} catch (SQLException e) {
-	    e.printStackTrace();
 	}
-	return columns;
-    }
 
-    public Properties getProperties() {
-	return properties;
-    }
+	public Config(String configFile) {
+		properties = new Properties();
+		try {
+			properties.load(new FileInputStream(new File(configFile)));
+			this._package = properties.getProperty("package");
+			this._extends = properties.getProperty("extends");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Connection getConnection() {
+		try {
+			String jdbcUrl = "jdbc:mysql://"
+					+ properties.getProperty("db.host") + ":"
+					+ properties.getProperty("db.port") + "/"
+					+ INFORMATION_SCHEMA + "?user="
+					+ properties.getProperty("db.user") + "&password="
+					+ properties.getProperty("db.password");
+			Connection connection = DriverManager.getConnection(jdbcUrl);
+			if (connection == null) {
+				System.out.println("Can't get connection");
+			}
+			return connection;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		return null;
+	}
+
+	private void close(Connection connection) {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getPackage() {
+		return this._package;
+	}
+
+	public String getExtends() {
+		return this._extends;
+	}
+
+	public void setPackage(String _package) {
+		this._package = _package;
+	}
+
+	public void setExtends(String _extends) {
+		this._extends = _extends;
+	}
+
+	public List<Table> getTables() {
+		Connection connection = this.getConnection();
+		String sql = "select * from TABLES where TABLE_SCHEMA=?";
+		List<Table> tables = null;
+		try {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement(sql);
+			preparedStatement.setString(1,
+					this.properties.getProperty("db.name"));
+			ResultSet rs = preparedStatement.executeQuery();
+			tables = new ArrayList<Table>();
+			while (rs.next()) {
+				Table table = new Table();
+				table.setName(rs.getString("TABLE_NAME").trim());
+				table.setComment(rs.getString("TABLE_COMMENT").trim());
+				table.setColumns(getColumns(table));
+				tables.add(table);
+			}
+			close(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return tables;
+	}
+
+	public Set<Column> getColumns(Table table) {
+		Connection connection = this.getConnection();
+		String sql = "select * from COLUMNS where TABLE_SCHEMA=? and TABLE_NAME=?";
+		Set<Column> columns = null;
+		try {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement(sql);
+			preparedStatement.setString(1,
+					this.properties.getProperty("db.name"));
+			preparedStatement.setString(2, table.getName());
+			ResultSet rs = preparedStatement.executeQuery();
+			columns = new HashSet<Column>();
+			boolean hasPriKey = false;
+			while (rs.next()) {
+				Column column = new Column();
+				column.setName(rs.getString("COLUMN_NAME").trim());
+				column.setDataType(rs.getString("DATA_TYPE"));
+				if (rs.getString("COLUMN_KEY").equalsIgnoreCase("PRI")) {
+					column.setPrimaryKey(true);
+					hasPriKey = true;
+				}
+				column.setComment(rs.getString("COLUMN_COMMENT").trim());
+				columns.add(column);
+			}
+			if (!hasPriKey) {
+				System.out.println("WARN: table(" + table.getName()
+						+ ") must have PRIMARY KEY. ");
+			}
+			close(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return columns;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public String getDomainSuffix() {
+		return _domainSuffix;
+	}
+
+	public void setDomainSuffix(String _domainSuffix) {
+		this._domainSuffix = _domainSuffix;
+	}
 }
--- src/main/java/com/xiebiao/tools/db/DomainClassBuilder.java
+++ src/main/java/com/xiebiao/tools/db/DomainClassBuilder.java
@@ -13,244 +13,211 @@ import java.util.Set;
  * 
  */
 public class DomainClassBuilder extends ClassBuilder {
-    private static String NAME_SUFFIX = "Domain";
-    private String _package;
-    private String tab;
-    private static final String OUTPUT = System.getProperty("user.dir")
-	    + File.separator + "output";
-    private Table table;
-    private StringBuffer sb;
-    private Config config;
+	private String _package;
+	private String tab;
+	private static final String OUTPUT = System.getProperty("user.dir")
+			+ File.separator + "output";
+	private Table table;
+	private StringBuffer sb;
+	private Config config;
 
-    private DomainClassBuilder(String _package) {
-	tab = "    ";
-	if (_package == null || _package.equals("")) {
-	    System.out.println("WARN: package must be setting. ");
-	}
-	this._package = _package;
-	File outputDir = new File(OUTPUT);
-	if (!outputDir.exists()) {
-	    outputDir.mkdir();
-	} else {
-	    FileUtils.deleteDirectory(OUTPUT);
-	}
-    }
-
-    public DomainClassBuilder(Config config) {
-	this(config.getPackage());
-	this.config = config;
-    }
-
-    public void buildStructure() {
-	sb.append("\n");
-	sb.append(tab + "public "
-		+ this.getModelClassName(table.getName() + NAME_SUFFIX)
-		+ "() {\n");
-	sb.append(tab + "}\n");
-    }
-
-    public DomainClassBuilder from(Table table) {
-	sb = new StringBuffer();
-	if (table == null || table.getName().equals("")) {
-	    throw new java.lang.IllegalArgumentException();
-	}
-	this.table = table;
-	return this;
-    }
-
-    protected void buildPackage() {
-	if (this._package != null && !this._package.equals("")) {
-	    sb.append("package " + this._package + ";");
-	    sb.append("\n");
-	}
-    }
-
-    protected void buildImport() {
-	if (sb.length() != 0) {
-	    sb.append("\n");
-	}
-	if (table.getColumns() == null) {
-	    return;
-	} else {
-	    for (Column c : table.getColumns()) {
-		if (DataType2Java.dataTypeMap.get(c.getDataType()).equals(
-			"Date")) {
-		    sb.append("import java.util.Date;\n");
-		    break;
+	private DomainClassBuilder(String _package) {
+		tab = "    ";
+		if (_package == null || _package.equals("")) {
+			System.out.println("WARN: package must be setting. ");
 		}
-	    }
-	}
-	sb.append("\n");
-    }
-
-    protected void buildAnnotate() {
-	sb.append("\n");
-	sb.append("/**\n");
-	sb.append(" * " + table.getComment() + "\n");
-	sb.append(" */");
-	sb.append("\n");
-    }
-
-    protected void buildClassName() {
-	sb.append("\n");
-	if (config.getExtends() != null) {
-	    sb.append("public class "
-		    + this.getModelClassName(table.getName() + NAME_SUFFIX)
-		    + " extends " + config.getExtends() + " {\n");
-	} else {
-	    sb.append("public class "
-		    + this.getModelClassName(table.getName() + NAME_SUFFIX)
-		    + " {\n");
-	}
-    }
-
-    protected void buildField() {
-	sb.append("\n");
-	Set<Column> columns = table.getColumns();
-	if (columns == null) {
-	    System.err.println("WARN: Table=" + table.getName()
-		    + " has no column.");
-	    return;
-	} else {
-	    for (Column c : columns) {
-		String name = getCamelName(this.getCamelName(c.getName()));
-		if (JavaKeyWord.isJavaKeyWord(name)) {
-		    name = "_" + name;
+		this._package = _package;
+		File outputDir = new File(OUTPUT);
+		if (!outputDir.exists()) {
+			outputDir.mkdir();
+		} else {
+			FileUtils.deleteDirectory(OUTPUT);
 		}
-		if (c.isPrimaryKey()) {
-		    sb.append(tab + "//primary key \n");
+	}
+
+	public DomainClassBuilder(Config config) {
+		this(config.getPackage());
+		this.config = config;
+	}
+
+	public void buildStructure() {
+		sb.append("\n");
+		sb.append(tab
+				+ "public "
+				+ this.getModelClassName(table.getName()
+						+ this.config.getDomainSuffix()) + "() {\n");
+		sb.append(tab + "}\n");
+	}
+
+	public DomainClassBuilder from(Table table) {
+		sb = new StringBuffer();
+		if (table == null || table.getName().equals("")) {
+			throw new java.lang.IllegalArgumentException();
 		}
-		sb.append(tab + "protected "
-			+ DataType2Java.dataTypeMap.get(c.getDataType()) + " "
-			+ name + ";\n");
-	    }
+		this.table = table;
+		return this;
 	}
-    }
 
-    protected void buildSetterGetter() {
-	sb.append("\n");
-	Set<Column> columns = table.getColumns();
-	for (Column c : columns) {
-	    String field = this.getCamelName(c.getName());
-	    String _field = field;
-	    if (JavaKeyWord.isJavaKeyWord(field)) {
-		_field = "_" + field;
-	    }
-	    sb.append("\n");
-	    sb.append(
-		    tab + "public void set"
-			    + field.substring(0, 1).toUpperCase()
-			    + field.substring(1, field.length()) + "("
-			    + DataType2Java.dataTypeMap.get(c.getDataType())
-			    + " " + _field + ")")
-		    .append(" {\n")
-		    .append(tab + tab + "this." + _field + " = " + _field
-			    + ";\n").append(tab + "}\n");
-	    sb.append("\n");
-	    sb.append(tab + "/**\n");
-	    sb.append(tab + " * " + c.getComment() + "\n");
-	    sb.append(tab + " */\n");
-	    sb.append(
-		    tab + "public "
-			    + DataType2Java.dataTypeMap.get(c.getDataType())
-			    + " get" + field.substring(0, 1).toUpperCase()
-			    + field.substring(1, field.length()) + "()")
-		    .append(" {\n").append(tab)
-		    .append(tab + "return this." + _field + ";\n")
-		    .append(tab + "}\n");
+	protected void buildPackage() {
+		if (this._package != null && !this._package.equals("")) {
+			sb.append("package " + this._package + ";");
+			sb.append("\n");
+		}
 	}
-    }
 
-    protected void buildClassEnd() {
-	sb.append("\n}");
-    }
-
-    public final void build() {
-	if (sb == null) {
-	    throw new java.lang.InstantiationError();
+	protected void buildImport() {
+		if (sb.length() != 0) {
+			sb.append("\n");
+		}
+		if (table.getColumns() == null) {
+			return;
+		} else {
+			for (Column c : table.getColumns()) {
+				if (DataType2Java.dataTypeMap.get(c.getDataType()).equals(
+						"Date")) {
+					sb.append("import java.util.Date;\n");
+					break;
+				}
+			}
+		}
+		sb.append("\n");
 	}
-	super.doBuild();
-	String dirPath = _package == null ? "" : _package;
-	if (_package != null && !_package.equals("")) {
-	    dirPath = dirPath.replace(".", File.separator);
-	    File dirs = new File(OUTPUT + File.separator + dirPath);
-	    if (!dirs.exists()) {
-		dirs.mkdirs();
-	    }
-	}
-	String modelClassName = getModelClassName(table.getName())
-		+ NAME_SUFFIX;
-	File modelFile = new File(OUTPUT + File.separator + dirPath
-		+ File.separator + modelClassName + ".java");
-	try {
-	    if (!modelFile.exists()) {
-		modelFile.createNewFile();
-	    }
-	    FileOutputStream out = new FileOutputStream(modelFile);
-	    out.write(sb.toString().getBytes());
-	    out.flush();
-	    out.close();
-	} catch (FileNotFoundException e) {
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-	System.out.println("build success!");
-	// Thread thread = new Thread(new CreateFiles());
-	// thread.start();
-    }
 
-    private String getModelClassName(String name) {
-	if (name.indexOf("-") != -1 && name.indexOf("_") != -1) {
-	    name.replaceAll("-", "_");
+	protected void buildAnnotate() {
+		sb.append("\n");
+		sb.append("/**\n");
+		sb.append(" * " + table.getComment() + "\n");
+		sb.append(" */");
+		sb.append("\n");
 	}
-	String[] t = name.split("_");
-	StringBuffer modelClassName = new StringBuffer();
-	for (String s : t) {
-	    modelClassName.append(s.substring(0, 1).toUpperCase()).append(
-		    s.substring(1, s.length()));
+
+	protected void buildClassName() {
+		sb.append("\n");
+		if (config.getExtends() != null) {
+			sb.append("public class "
+					+ this.getModelClassName(table.getName()
+							+ this.config.getDomainSuffix()) + " extends "
+					+ config.getExtends() + " {\n");
+		} else {
+			sb.append("public class "
+					+ this.getModelClassName(table.getName()
+							+ this.config.getDomainSuffix()) + " {\n");
+		}
 	}
-	return modelClassName.toString();
-    }
 
-    private String getCamelName(String name) {
-	name = getModelClassName(name);
-	name = name.substring(0, 1).toLowerCase()
-		+ name.substring(1, name.length());
-	return name;
-    }
+	protected void buildField() {
+		sb.append("\n");
+		Set<Column> columns = table.getColumns();
+		if (columns == null) {
+			System.err.println("WARN: Table=" + table.getName()
+					+ " has no column.");
+			return;
+		} else {
+			for (Column c : columns) {
+				String name = getCamelName(this.getCamelName(c.getName()));
+				if (JavaKeyWord.isJavaKeyWord(name)) {
+					name = "_" + name;
+				}
+				if (c.isPrimaryKey()) {
+					sb.append(tab + "//primary key \n");
+				}
+				sb.append(tab + "protected "
+						+ DataType2Java.dataTypeMap.get(c.getDataType()) + " "
+						+ name + ";\n");
+			}
+		}
+	}
 
-    class CreateFiles implements Runnable {
+	protected void buildSetterGetter() {
+		sb.append("\n");
+		Set<Column> columns = table.getColumns();
+		for (Column c : columns) {
+			String field = this.getCamelName(c.getName());
+			String _field = field;
+			if (JavaKeyWord.isJavaKeyWord(field)) {
+				_field = "_" + field;
+			}
+			sb.append("\n");
+			sb.append(
+					tab + "public void set"
+							+ field.substring(0, 1).toUpperCase()
+							+ field.substring(1, field.length()) + "("
+							+ DataType2Java.dataTypeMap.get(c.getDataType())
+							+ " " + _field + ")")
+					.append(" {\n")
+					.append(tab + tab + "this." + _field + " = " + _field
+							+ ";\n").append(tab + "}\n");
+			sb.append("\n");
+			sb.append(tab + "/**\n");
+			sb.append(tab + " * " + c.getComment() + "\n");
+			sb.append(tab + " */\n");
+			sb.append(
+					tab + "public "
+							+ DataType2Java.dataTypeMap.get(c.getDataType())
+							+ " get" + field.substring(0, 1).toUpperCase()
+							+ field.substring(1, field.length()) + "()")
+					.append(" {\n").append(tab)
+					.append(tab + "return this." + _field + ";\n")
+					.append(tab + "}\n");
+		}
+	}
 
-	public void run() {
-	    synchronized (sb) {
+	protected void buildClassEnd() {
+		sb.append("\n}");
+	}
+
+	public final void build() {
+		if (sb == null) {
+			throw new java.lang.InstantiationError();
+		}
+		super.doBuild();
 		String dirPath = _package == null ? "" : _package;
 		if (_package != null && !_package.equals("")) {
-		    dirPath = dirPath.replace(".", File.separator);
-		    File dirs = new File(OUTPUT + File.separator + dirPath);
-		    if (!dirs.exists()) {
-			dirs.mkdirs();
-		    }
+			dirPath = dirPath.replace(".", File.separator);
+			File dirs = new File(OUTPUT + File.separator + dirPath);
+			if (!dirs.exists()) {
+				dirs.mkdirs();
+			}
 		}
 		String modelClassName = getModelClassName(table.getName())
-			+ NAME_SUFFIX;
+				+ this.config.getDomainSuffix();
 		File modelFile = new File(OUTPUT + File.separator + dirPath
-			+ File.separator + modelClassName + ".java");
+				+ File.separator + modelClassName + ".java");
 		try {
-		    if (!modelFile.exists()) {
-			modelFile.createNewFile();
-		    }
-		    FileOutputStream out = new FileOutputStream(modelFile);
-		    out.write(sb.toString().getBytes());
-		    out.flush();
-		    out.close();
+			if (!modelFile.exists()) {
+				modelFile.createNewFile();
+			}
+			FileOutputStream out = new FileOutputStream(modelFile);
+			out.write(sb.toString().getBytes());
+			out.flush();
+			out.close();
 		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		} catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
-		sb = new StringBuffer();
-	    }
+		System.out.println("build success!");
+		// Thread thread = new Thread(new CreateFiles());
+		// thread.start();
 	}
-    }
+
+	private String getModelClassName(String name) {
+		if (name.indexOf("-") != -1 && name.indexOf("_") != -1) {
+			name.replaceAll("-", "_");
+		}
+		String[] t = name.split("_");
+		StringBuffer modelClassName = new StringBuffer();
+		for (String s : t) {
+			modelClassName.append(s.substring(0, 1).toUpperCase()).append(
+					s.substring(1, s.length()));
+		}
+		return modelClassName.toString();
+	}
+
+	private String getCamelName(String name) {
+		name = getModelClassName(name);
+		name = name.substring(0, 1).toLowerCase()
+				+ name.substring(1, name.length());
+		return name;
+	}
 }
